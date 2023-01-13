30314a2c46fe1e19f0ab2bf1a818ecd483684d0d
Giap <ryon@conjurers.dev>
2023-01-13 15:08:46 +0800

    - Initial setups and project structuring

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Giap on 13/01/2023.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
