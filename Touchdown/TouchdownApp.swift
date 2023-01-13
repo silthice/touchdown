761e9dc1bf7fc9daaa1847afdbfb03aa59edbb72
Giap <ryon@conjurers.dev>
2023-01-13 14:49:58 +0800

    -initial commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Giap on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
